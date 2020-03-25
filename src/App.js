ce5cf59a8f1951d61e76a8f3d7b66589674098dc
agavrel <gavrel.antonin@gmail.com>
2020-03-25 20:29:26 +0100

    [INIT] Project for img processing

---
+++ src/App.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Progress} from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null,
+        loaded:0
+      }
+
+  }
+  checkMimeType=(event)=>{
+    //getting file object
+    let files = event.target.files
+    //define message container
+    let err = []
+    // list allow mime type
+   const types = ['image/png', 'image/jpeg', 'image/gif']
+    // loop access array
+    for(var x = 0; x<files.length; x++) {
+     // compare file type find doesn't matach
+         if (types.every(type => files[x].type !== type)) {
+         // create error message and assign to container
+         err[x] = files[x].type+' is not a supported format\n';
+       }
+     };
+     for(var z = 0; z<err.length; z++) {// if message not same old that mean has error
+         // discard selected file
+        toast.error(err[z])
+        event.target.value = null
+    }
+   return true;
+  }
+  maxSelectFile=(event)=>{
+    let files = event.target.files
+        if (files.length > 3) {
+           const msg = 'Only 3 images can be uploaded at a time'
+           event.target.value = null
+           toast.warn(msg)
+           return false;
+      }
+    return true;
+ }
+ checkFileSize=(event)=>{
+  let files = event.target.files
+  let size = 2000000
+  let err = [];
+  for(var x = 0; x<files.length; x++) {
+  if (files[x].size > size) {
+   err[x] = files[x].type+'is too large, please pick a smaller file\n';
+ }
+};
+for(var z = 0; z<err.length; z++) {// if message not same old that mean has error
+  // discard selected file
+ toast.error(err[z])
+ event.target.value = null
+}
+return true;
+}
+onChangeHandler=event=>{
+  var files = event.target.files
+  if(this.maxSelectFile(event) && this.checkMimeType(event) &&    this.checkFileSize(event)){
+  // if return true allow to setState
+     this.setState({
+     selectedFile: files,
+     loaded:0
+  })
+}
+}
+  onClickHandler = () => {
+    const data = new FormData()
+    for(var x = 0; x<this.state.selectedFile.length; x++) {
+      data.append('file', this.state.selectedFile[x])
+    }
+    axios.post("http://localhost:8000/upload", data, {
+      onUploadProgress: ProgressEvent => {
+        this.setState({
+          loaded: (ProgressEvent.loaded / ProgressEvent.total*100),
+        })
+      },
+    })
+      .then(res => { // then print response status
+        toast.success('upload success')
+      })
+      .catch(err => { // then print response status
+        toast.error('upload fail')
+      })
+    }
+
+  render() {
+    return (
+      <div class="container">
+	      <div class="row">
+      	  <div class="offset-md-3 col-md-6">
+               <div class="form-group files">
+                <label>Upload Your File </label>
+                <input type="file" class="form-control" multiple onChange={this.onChangeHandler}/>
+              </div>
+              <div class="form-group">
+              <ToastContainer />
+              <Progress max="100" color="success" value={this.state.loaded} >{Math.round(this.state.loaded,2) }%</Progress>
+
+              </div>
+
+              <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+
+	      </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+/*import React from 'react';
+import './App.css';
+import DragAndDrop from './DragAndDrop'
+
+
+
+function ProgressHandler(e) {
+	var complete = Math.round(e.loaded / e.total * 100);
+	console.log(complete + "% complete");
+}
+
+function App() {
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_DROP_DEPTH':
+        return { ...state, dropDepth: action.dropDepth }
+      case 'SET_IN_DROP_ZONE':
+        return { ...state, inDropZone: action.inDropZone };
+      case 'ADD_FILE_TO_LIST':
+        return { ...state, fileList: state.fileList.concat(action.files) };
+      default:
+        return state;
+    }
+  };
+
+  const [data, dispatch] = React.useReducer(
+    reducer, { dropDepth: 0, inDropZone: false, fileList: [] }
+  )
+
+  return (
+    <div className="App">
+      <h1>React drag and drop component</h1>
+      <DragAndDrop data={data} dispatch={dispatch} />
+
+      <ol className="dropped-files">
+        {data.fileList.map(f => {
+          return (
+            <li key={f.name}>{f.name}</li>
+          )
+        })}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
+*/
